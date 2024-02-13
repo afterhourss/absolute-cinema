b8dee12a0bfe34a75ba105a8e65af2b06362e988
afterhourss <adiptabagasss@gmail.com>
2024-02-13 19:24:26 +0700

    fix few things

--- src/Tvseries.jsx
+++ src/Tvseries.jsx
@@ -2,20 +2,22 @@ import Card from "./components/Card"
 import { useState, useEffect } from "react"
 import options from "./api"
 import Navbar from "./components/Navbar"
+import Pagination from "./components/Pagination"
 
 function Tvseries() {
 
   const [tvSeries, setTvSeries] = useState([])
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
-    fetch('https://api.themoviedb.org/3/tv/popular?language=en-US&page=1', options)
+    fetch(`https://api.themoviedb.org/3/tv/popular?language=en-US&page=${currentPage}`, options)
       .then(response => response.json())
       .then(data => {
         setTvSeries(data)
         console.log(data)
       })
         .catch(err => console.log(err))
-  }, [])
+  }, [currentPage])
 
   return (
     <>
@@ -23,6 +25,7 @@ function Tvseries() {
     <div className="container">
         <p>List by Tv Series</p>
         <Card movies={tvSeries}/>
+        <Pagination data={tvSeries} setPage={setCurrentPage} currPage={currentPage}/>
     </div>
     </>
   )
--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -2,7 +2,7 @@ function Searchbar() {
   return (
     <div className="searchbar">
         <form action="">
-            <span>🔎</span><input type="text" placeholder="Fight Club" />
+            <input type="text" placeholder="Fight Club" />
         </form>
     </div>
   )
