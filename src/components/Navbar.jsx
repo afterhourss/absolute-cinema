87a686df264018f08922618ca2299c957df0153c
afterhourss <adiptabagasss@gmail.com>
2024-02-15 01:40:22 +0700

    add animation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,19 +4,21 @@ import { IoMdMenu } from "react-icons/io";
 import { Link } from "react-router-dom";
 import Searchbar from "./Searchbar";
 import { FaGithub } from "react-icons/fa";
-import { MdOutlineLightMode } from "react-icons/md";
+import { MdOutlineLightMode, MdDarkMode } from "react-icons/md";
+import { motion } from "framer-motion"
 
 function Navbar({darkTheme, setDarkTheme}) {
 
     const [hiddenBar, setHiddenBar] = useState(true);
 
+
   return (
     <>
-    <div className={`navbar-container ${darkTheme ? "dark" : ""}`}>
+    <div className="navbar-container">
         <div className="navbar-left">
-            <div className="icon" onClick={() => setHiddenBar(!hiddenBar)}>
+            <motion.div whileHover={{scale: 1.1}} whileTap={{scale: 0.9}} className="icon" onClick={() => setHiddenBar(!hiddenBar)}>
                 <IoMdMenu />
-            </div>
+            </motion.div>
             <h1>Absc</h1>
         </div>
         <div className="navbar-right">
@@ -27,10 +29,12 @@ function Navbar({darkTheme, setDarkTheme}) {
             </ul>
             <Searchbar/>
             <FaGithub className="icon"/>
-            <MdOutlineLightMode className="icon" onClick={() => setDarkTheme(!darkTheme)}/>
+            <motion.div whileHover={{scale: 1.2}} whileTap={{scale: 0.9}} onClick={() => setDarkTheme(!darkTheme)}>
+                {darkTheme ? <MdOutlineLightMode className="icon" /> : <MdDarkMode className="icon"/>}
+            </motion.div>
         </div>
     </div>
-    <Sidebar isHidden={hiddenBar} setHidden={setHiddenBar}/>
+    <Sidebar isHidden={hiddenBar} setHidden={setHiddenBar} darkTheme={darkTheme} setDarkTheme={setDarkTheme}/>
     </>
   )
 }
