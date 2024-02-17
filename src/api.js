5b3ebd1b700f831c34b353b0dc6ef68d6923319b
afterhourss <adiptabagasss@gmail.com>
2024-02-17 19:27:12 +0700

    api fix

--- src/api.js
+++ src/api.js
@@ -1,10 +1,8 @@
-import 'dotenv/config'
-
 const options = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NGFjYTk5M2IxYmJkMTcyMWNhYTNiOWNhYmViOGIyZCIsInN1YiI6IjY1YzM2ZjJjZTI2N2RlMDE0OGJiM2Y5OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dUyTGh2seHbIgNgOYPy2F1-LjieFHhOIuNM4tTbWcj8'
   }
 };
 
