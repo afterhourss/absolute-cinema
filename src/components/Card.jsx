7deb8d0036e8db633dd85011c22a2f338ca99977
afterhourss <adiptabagasss@gmail.com>
2024-02-18 19:50:45 +0700

    giving null value when api json not giving any objects, to prevent no poster overview not showing

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -26,11 +26,11 @@ function Card({movies, type}) {
       {movies.results?.map(item => {
       return <motion.div whileHover={{scale: 1.1}} whileTap={{scale: 0.9}} className="card" key={item.id} onClick={() => handleClick(item)}>
           <div className="card-image">
-              <img src={getPoster(item.poster_path)} alt="" />
+              <img src={getPoster(item.poster_path || null)} alt="" />
           </div>
       </motion.div>
       })}
-      <Info id={activeInfo.id} type={activeInfo.media_type || type} title={activeInfo.title || activeInfo.name} desc={activeInfo.overview} posters={getPoster(activeInfo.poster_path)} isShow={showInfo} setShow={setShowInfo} rating={activeInfo.vote_average?.toFixed(1)} date={activeInfo.release_date?.split('-')[0]} genre_id={activeInfo.genre_ids} language={activeInfo.original_language}/>
+      <Info id={activeInfo.id} type={activeInfo.media_type || type} title={activeInfo.title || activeInfo.name} desc={activeInfo.overview} posters={getPoster(activeInfo.poster_path || null)} isShow={showInfo} setShow={setShowInfo} rating={activeInfo.vote_average?.toFixed(1)} date={activeInfo.release_date?.split('-')[0]} genre_id={activeInfo.genre_ids} language={activeInfo.original_language}/>
     </div>
   )
 }
